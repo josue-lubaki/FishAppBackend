320115ed1309053de208b844de78faf250572aaf
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-07-12 06:47:04 -0400

    amelioration tests

--- test/api/categorie/test.js
+++ test/api/categorie/test.js
@@ -4,6 +4,7 @@ const request = require('supertest')
 const app = require('../../../app')
 const database = require('../../../db/database')
 const api = process.env.API_URL
+const authJwt = require('../../../helpers/jwt')
 
 describe('GET /categories', () => {
     before((done) => {
@@ -30,7 +31,46 @@ describe('GET /categories', () => {
             })
             .catch((err) => done(err))
     })
-    
+
+    it('OK, creating a new Categorie works', (done) => {
+        request(app)
+            .post(`${api}/categories/`, authJwt)
+            .send({
+                name: 'Categorie test',
+                icon: 'icon test',
+                color: 'color test',
+            })
+            .then((res) => {
+                const body = res.body
+                expect(body).that.includes.all.keys([
+                    '_id',
+                    'name',
+                    'icon',
+                    'color',
+                ])
+                expect(200, done)
+                done()
+            })
+            .catch((err) => done(err))
+    })
+
+    it('OK, deleting a Categorie works', (done) => {
+        request(app)
+            .get(`${api}/categories/`)
+            .then((res) => {
+                const body = res.body
+                request(app)
+                    .delete(`${api}/categories/${body.pop()._id}`, authJwt)
+                    .then((res) => {
+                        expect(res.body.length).to.equal(0)
+                        done()
+                    })
+                expect(body.length).to.equal(0)
+                done()
+            })
+            .catch((err) => done(err))
+    })
+
     it('OK, getting categories has 1 categorie', (done) => {
         request(app)
             .post(`${api}/categories/`)
--- test/api/user/test.js
+++ test/api/user/test.js
@@ -31,6 +31,42 @@ describe('GET /users', () => {
             .catch((err) => done(err))
     })
 
+    it('OK, creating a new users works', (done) => {
+        request(app)
+            .post(`${api}/users/`)
+            .send({
+                name: 'user test',
+                isAdmin: 'false',
+                avenue: 'avenue test',
+                apartment: '2',
+                quartier: 'quartier test',
+                commune: 'commune test',
+                city: 'city test',
+                email: 'email test',
+                password: 'passwordHash',
+                phone: 'phone',
+            })
+            .then((res) => {
+                const body = res.body
+                expect(body).that.includes.all.keys([
+                    '_id',
+                    'name',
+                    'isAdmin',
+                    'avenue',
+                    'apartment',
+                    'quartier',
+                    'commune',
+                    'city',
+                    'email',
+                    'passwordHash',
+                    'phone',
+                ])
+                expect(200, done)
+                done()
+            })
+            .catch((err) => done(err))
+    })
+
     it('OK, getting users has 1 user', (done) => {
         request(app)
             .post(`${api}/users/`)
@@ -51,7 +87,7 @@ describe('GET /users', () => {
                     .get(`${api}/users/`)
                     .then((res) => {
                         const body = res.body
-                        expect(body.length).to.equal(1)
+                        expect(body.length).to.equal(2)
                         done()
                     })
             })
