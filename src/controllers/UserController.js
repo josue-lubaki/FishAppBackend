2ec24d304799125b86b9eb725c88d9cadb4b5340
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-10-21 04:53:14 -0400

    Update UserController.js

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -433,7 +433,7 @@ module.exports = {
             client.messages
                 .create({
                     body: `
-                    - ${WEBSITE} - 
+                    - poisson-kin.com - 
 Merci Beaucoup pour votre confiance en notre équipe.
 Nous vous avisons que votre mot de passe vient d'être modifier, si vous êtes l'auteur de cette modification ignore ce message.
 
